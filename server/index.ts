7cba2fd24df7f348415c8312c642e37e34b01957
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-07 20:06:53 +0530

    index.ts: established mongoDB connection

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,42 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import cors from "cors"
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL! as string;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const connect = async () => {
+    try {
+        if (!MONGO_URL) {
+            console.log("MongoDB URL is not defined")
+        }
+
+        await mongoose.connect(MONGO_URL);
+        console.log("Connected to DB");
+
+        app.listen(PORT, () => {
+            console.log(`Server is live on port ${PORT}`)
+        })
+
+    } catch (error) {
+        console.log(`MongoDB connection error ${error}`);
+        process.exit(1);
+    }
+}
+
 
 app.get("/", (req, res) => {
     res.send("ai-spend-audit");
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
-})
+connect();
