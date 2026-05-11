d8197084b53b40a2196952df979958667ab566c0
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-11 14:07:30 +0530

    fix(server): remove unused mongoose import in index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+
 import cors from "cors";
 import { connectDB } from "./db/connect.js";
 import auditRoute from "./routes/audit.js";
