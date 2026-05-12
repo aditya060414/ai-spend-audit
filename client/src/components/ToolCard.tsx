b2a09cc2f762f004abd3abeb4813f76ba7a9c059
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-12 06:32:28 +0530

    perf: remove layout prop from ToolCard to prevent forced reflows

--- client/src/components/ToolCard.tsx
+++ client/src/components/ToolCard.tsx
@@ -20,7 +20,6 @@ export function ToolCard({
 }: ToolCardProps) {
   return (
     <motion.div
-      layout
       initial={{ opacity: 0, y: 10, scale: 0.98 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, scale: 0.95, transition: { duration: 0.2 } }}
